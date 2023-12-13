6ebd956ce4e1f2b0fde799cc24b7fd99460cf26a
DiegoAAG-aoe <diegoalvarez534@gmail.com>
2023-12-13 16:06:00 -0300

    new refactor

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  delete '/productos/:id', to: 'productos#destroy'
-  patch '/productos/:id', to: 'productos#update'
-  post '/productos', to: 'productos#create'
-  get '/productos/new', to: 'productos#new', as: :new_producto
-  get '/productos', to: 'productos#index'
-  get '/productos/:id', to: 'productos#show', as: :producto
-  get '/productos/:id/edit', to: 'productos#edit', as: :edit_producto
+  resources :productos
 end
