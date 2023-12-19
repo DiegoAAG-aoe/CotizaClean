70bf3ba9f7a811a3ebf6288537fc5f9e7c9a0d45
juasjuasjuas <69996441+juasjuasjuas@users.noreply.github.com>
2023-12-18 22:20:11 -0300

    repaired the login, home, and credential issues
    above

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
   resources :categories, except: :show
-  resources :productos, path: '/'
+  resources :productos, path: '/productos'
 end
