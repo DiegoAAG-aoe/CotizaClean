763c2e081066a2fe576d2d7ef42ea33bfad1735b
DiegoAAG-aoe <diegoalvarez534@gmail.com>
2023-12-21 07:51:20 -0300

    arregllo

--- db/schema.rb
+++ db/schema.rb
@@ -43,7 +43,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_20_232947) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -59,13 +59,20 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_20_232947) do
   end
 
   create_table "gestors", force: :cascade do |t|
-    t.text "Nombre"
-    t.string "Tipo"
-    t.integer "Precio"
-    t.text "Descripcion"
-    t.text "Marca"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "full_name"
+    t.string "uid"
+    t.string "avatar_url"
+    t.string "provider"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "tipo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_gestors_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_gestors_on_reset_password_token", unique: true
   end
 
   create_table "productos", force: :cascade do |t|
